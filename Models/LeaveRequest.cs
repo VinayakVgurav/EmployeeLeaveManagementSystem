8e6b3257ad4341f5fe52b7f0935ca928653e1c2d
Vinayak Gurav <vinayakgurav0904@gmail.com>
2025-09-09 17:17:51 +0530

    Created models and done migration .

---
+++ Models/LeaveRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeLeaveManagementSystem.Models
+{
+    public class LeaveRequest
+    {
+        [Key]
+        public int LeaveRequestID { get; set; }
+
+        [ForeignKey("Employee")]
+        public int EmployeeID { get; set; }
+        public string LeaveType { get; set; } 
+        public DateTime StartDate { get; set; } 
+        public DateTime EndDate { get; set; }
+        public string Reason { get; set; } 
+        public string Status { get; set; } 
+        public string AdminRemarks { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
---
+++ Models/Mydbcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeLeaveManagementSystem.Models
+{
+    public class Mydbcontext:DbContext
+    {
+        public Mydbcontext(DbContextOptions<Mydbcontext>options):base(options)
+        { 
+
+        }   
+        
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<LeaveBalance> LeaveBalances { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+    }
+}
