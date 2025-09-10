617a75b6503e5a3c25d88a4db06de2691971ac85
komalkre1993 <komalkore1993@gmail.com>
2025-09-10 18:06:20 +0530

    changes

--- Models/DTOs/RegisterDTO.cs
+++ Models/DTOs/RegisterDTO.cs
@@ -2,5 +2,11 @@
 {
     public class RegisterDTO
     {
+        //string.Empty is just a constant in .NET equal to "" (empty string).
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string Designation { get; set; } = string.Empty;
     }
 }
