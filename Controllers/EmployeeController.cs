684693e34ae06996fa81e3614c8cfb04de2ac980
komalkre1993 <komalkore1993@gmail.com>
2025-09-10 16:50:36 +0530

    emp controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeLeaveManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
