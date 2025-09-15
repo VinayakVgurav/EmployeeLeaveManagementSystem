85d41dac3c2e8dd16a7d53ba0290713ff3436973
komalkre1993 <komalkore1993@gmail.com>
2025-09-15 17:47:46 +0530

    JwtHelper file updated

--- Helpers/JwtHelper.cs
+++ Helpers/JwtHelper.cs
@@ -1,6 +1,48 @@
-﻿namespace EmployeeLeaveManagementSystem.Helpers
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EmployeeLeaveManagementSystem.Helpers
 {
     public static class JwtHelper
     {
+        //Generates a JWT token for a given user.
+        //name=The name of the user (stored in token claims)
+        //role=The role of the user (Admin / Employee)
+        //IConfiguration=Application configuration (used to read JWT settings from appsettings.json)
+        //A signed JWT token as a string
+
+        public static string GenerateToken(string name,string role,IConfiguration config)
+        {
+            //Define claims (pieces of info about the user stored inside the token)
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, name),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            //Create a secret key (read from appsettings.json)
+            // This key is used to digitally sign the token
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+
+            //Define signing credentials(which algorithm and key to use)
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //Create the JWT token object
+           var token = new JwtSecurityToken(
+                issuer: config["Jwt:Issuer"],      // Who issued the token (your app)
+                audience: config["Jwt:Audience"],  // Who can use the token (your client)
+                claims: claims,                    // User info (name + role)
+                expires: DateTime.Now.AddHours(1), // Expiration time (1 hour from now)
+                signingCredentials: creds);        //// Digital signature for security
+
+
+            //Convert the JWT object into a string (the actual token sent to client)
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
